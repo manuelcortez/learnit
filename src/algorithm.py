eba6565839ab444d87367d5071cc6e2aea0bfe3f
Manuel Cortez <manuel@manuelcortez.net>
2017-11-16 17:30:42 -0600

    Fixed style and added references to the algorithm

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,14 +1,16 @@
-def interval(repition, rating, easy_factor=2.5):
-    ef = easy_factor + (0.1 - (5 - rating) * (0.08 + (5 - rating) * 0.02))
-    ef = ef if ef >= 1.3 else 1.3
+# -*- coding: utf-8 -*-
+import datetime
 
-    if rating < 3:
-        return 1, ef
-    if repition == 1:
-        return 1, ef
-    if repition == 2:
-        return 6, ef
-
-    i, ef = interval(repition-1, rating, easy_factor)
-    i *= easy_factor
-    return i, ef
+def interval(repetition, rating, easy_factor=2.5):
+	""" SM2 algorithm: https://www.supermemo.com/english/ol/sm2.htm"""
+	ef = easy_factor + (0.1 - (5 - rating) * (0.08 + (5 - rating) * 0.02))
+	ef = ef if ef >= 1.3 else 1.3
+	if rating < 3:
+		return 1, ef
+	if repetition == 1:
+		return 1, ef
+	if repetition == 2:
+		return 6, ef
+	i, ef = interval(repetition-1, rating, easy_factor)
+	i *= easy_factor
+	return i, ef
