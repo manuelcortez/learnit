bf18706d914a99c1d75089497e7e11850e55362d
Manuel Cortez <manuel@manuelcortez.net>
2017-11-29 17:41:04 -0600

    Added user registration

--- controllers/default.py
+++ controllers/default.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
 
+def user():
+	if len(request.args) == 0:
+		return redirect(URL("default", "index"))
+	arg = request.args[0]
+	if arg == "register":
+		response.title = T("Register")
+	elif arg == "login":
+		response.title = T("Log-in")
+	return dict(form=auth(), share=False)
+
+@cache.action()
+def download():
+	return response.download(request, db)
+
 def index():
 	response.title = "Learn It"
 	return dict()
